b60c24d61141a14be0909fba2b345c49f7988a15
Doug Twitchell <twitched@gmail.com>
2021-09-04 19:39:14 -0600

    fix list_course_users

--- list_course_users.py
+++ list_course_users.py
@@ -5,32 +5,36 @@ import argparse, csv
 
 import localcanvasapi
 
-localcanvasapi.debug() 
-
 def main():
     args = getargparser().parse_args()
     canvas = localcanvasapi.startcanvasapi(args)
-    print(args)
+    enrollment_types = []
+    if(args.students):
+        enrollment_types.append('student')
+    if(args.test_student):
+        enrollment_types.append('student_view')
     if(args.file):
-        save_users(canvas, args.course, args.file)
+        save_users(canvas, args.course, args.file, enrollment_types)
     else:
-        list_users(canvas, args.course)
+        list_users(canvas, args.course, enrollment_types)
     
 def getargparser():
     p = localcanvasapi.get_argparser()
     p.description = "List all of the assignments in this course"
     p.add_argument('-c', '--course', required=True, help='The course id whose events will be listed or deleted')
+    p.add_argument('-t', '--test_student', action='store_true', help='include the test student')
+    p.add_argument('--students', action='store_true', help='only include students')
     p.add_argument('-f', '--file', type=argparse.FileType('w', encoding='utf-8'), help="If present, all student information will be saved as a csv to the given file")
     return p
 
-def list_users(canvas: Canvas, course_id: str):
+def list_users(canvas: Canvas, course_id: str, enrollment_types: list[str]):
     course = canvas.get_course(course_id)
-    for a in course.get_users():
+    for a in course.get_users(enrollment_type = enrollment_types):
         print(a)
         
-def save_users(canvas: Canvas, course_id: str, file: IO):
+def save_users(canvas: Canvas, course_id: str, file: IO, enrollment_types: list[str]):
     course = canvas.get_course(course_id)
-    users = course.get_users()
+    users = course.get_users(enrollment_type = enrollment_types)
     first = True
     out = csv.DictWriter(file, ['id', 'name', 'created_at', 'sortable_name', 'short_name', 'sis_user_id', 'integration_id', 'login_id', 'email', 'pronouns'])
     out.writeheader()
