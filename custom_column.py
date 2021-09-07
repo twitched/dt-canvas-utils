c66fcf9bbabe3f289530aaa444535dbe0b0967c2
Doug Twitchell <twitched@gmail.com>
2021-09-07 08:20:54 -0600

    Add option to designate the content column

--- custom_column.py
+++ custom_column.py
@@ -1,17 +1,22 @@
 from canvasapi import Canvas
 import localcanvasapi, csv, time
 
+localcanvasapi.debug()
+
 def main():
     args = getargparser().parse_args()
     canvas = localcanvasapi.startcanvasapi(args)
+    content_column = 'content'
+    if(args.content_column):
+        content_column = args.content_column
     if(args.list):
         list_columns(canvas, args.course)
     elif(args.new):
-        new_column(canvas, args.course, args.new, args.file)
+        new_column(canvas, args.course, args.new, args.file, content_column)
     elif(args.delete):
         delete_column(canvas, args.course, args.delete)
     elif(args.update):
-        update_column(canvas, args.course, args.update, args.file)
+        update_column(canvas, args.course, args.update, args.file, content_column)
     
 def getargparser():
     p = localcanvasapi.get_argparser()
@@ -22,7 +27,8 @@ def getargparser():
     g.add_argument('-n', '--new', help='create a new grade column with the given title')
     g.add_argument('-d', '--delete', help='delete the grade column with the given title')
     g.add_argument('-u', '--update', help='update the grade column with the given title')
-    p.add_argument('-f', '--file', help='a csv file containing the column data for --new and --update.  It should have two columns: user_id, content')
+    p.add_argument('-f', '--file', help='a csv file containing the column data for --new and --update.  It should have two columns: id, content')
+    p.add_argument('-o', '--content_column', help='change the "content" column to the one specified')
     return p
 
 def list_columns(canvas: Canvas, course_id: str):
@@ -30,11 +36,11 @@ def list_columns(canvas: Canvas, course_id: str):
     for a in course.get_custom_columns():
         print(a)
         
-def new_column(canvas: Canvas, course_id: str, title: str, file: str):
+def new_column(canvas: Canvas, course_id: str, title: str, file: str, content_column: str):
     course = canvas.get_course(course_id)
     course.create_custom_column(column={'title':title, 'read_only': 'true'})
     if(file):
-        update_column(canvas, course.id, title, file)
+        update_column(canvas, course.id, title, file, content_column)
 
 def find_column_by_name(canvas: Canvas, course, title: str):
     for a in course.get_custom_columns():
@@ -50,7 +56,7 @@ def delete_column(canvas: Canvas, course_id: str, title: str):
     else:
         print(f'could not find {column}')
 
-def update_column(canvas: Canvas, course_id: str, title: str, file: str):
+def update_column(canvas: Canvas, course_id: str, title: str, file: str, content_column: str):
     course = canvas.get_course(course_id)
     column = find_column_by_name(canvas, course, title)
     if(column):
@@ -58,7 +64,7 @@ def update_column(canvas: Canvas, course_id: str, title: str, file: str):
             reader = csv.DictReader(csvfile)
             bulk_column_data = []
             for row in reader:
-                row_data = {'column_id': column.id, 'user_id': row['id'], 'content': row['content']}
+                row_data = {'column_id': column.id, 'user_id': row['id'], 'content': row[content_column]}
                 bulk_column_data.append(row_data)
         progress = course.column_data_bulk_update(bulk_column_data)
         while(progress.workflow_state != "completed"):
