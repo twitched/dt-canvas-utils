fd098ff6183d2d952aaa709c12ab9340e86e4c8e
Doug Twitchell <twitched@gmail.com>
2021-09-08 14:59:08 -0600

    make download_submissions work

--- download_submissions_for_assignment.py
+++ download_submissions_for_assignment.py
@@ -1,9 +1,11 @@
 # Import the Canvas class
-from canvasapi import Canvas, exceptions
+from canvasapi import Canvas
+from pathlib import Path
+import urllib, zipfile, shutil
 
 import localcanvasapi
 
-localcanvasapi.debug()
+#localcanvasapi.debug()
 
 def main():
     args = getargparser().parse_args()
@@ -19,9 +21,30 @@ def getargparser():
 
 def download_submissions(canvas: Canvas, course_id: str, assignment_id: str):
     course = canvas.get_course(course_id)
-    for s in course.get_assignment(assignment_id).get_submissions():
-        print(s)
-    #Doesn't do what it should tathis point.
-    
+    assignment = course.get_assignment(assignment_id)
+    for s in assignment.get_submissions(include=['user']):
+        if(s.attempt):
+            print(vars(s))
+            username = s.user['name']
+            path = Path(f'{assignment.name}/{username}')
+            path.mkdir(parents=True, exist_ok=True) #make a directory with the assignment name and username
+            if(hasattr(s, 'attachments')):
+                for a in s.attachments:
+                    filepath = path.joinpath(a['filename'])
+                    with urllib.request.urlopen(a['url']) as response, open(filepath, 'wb') as out_file:
+                        data = response.read() # a `bytes` object
+                        out_file.write(data)
+                    if(a['mime_class'] == 'zip'):
+                        with zipfile.ZipFile(filepath, 'r') as zip_ref:
+                            zip_ref.extractall(path)
+                        #copy exe, txt, and form1.cs
+                        copy_files_in_tree_to_top(path, "**/bin/**/*.exe")
+                        copy_files_in_tree_to_top(path, "**/bin/**/*.txt")
+                        copy_files_in_tree_to_top(path, "**/Form1.cs")
+
+def copy_files_in_tree_to_top(path: Path, glob: str):
+    for f in list(path.glob(glob)):
+        shutil.copy(f,path)
+
 if __name__ == '__main__':
    main()
