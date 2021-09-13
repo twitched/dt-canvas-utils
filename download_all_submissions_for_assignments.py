e5411645bf3a10a8926bab93aff2637c662e02c4
Doug Twitchell <twitched@gmail.com>
2021-09-13 17:33:47 -0600

    Allow getting submissions by user

--- download_all_submissions_for_assignments.py
+++ download_all_submissions_for_assignments.py
@@ -7,20 +7,21 @@ import download_submissions_for_assignment
 def main():
     args = getargparser().parse_args()
     canvas = localcanvasapi.startcanvasapi(args)
-    download_submissions(canvas, args.course, args.starts_with)
+    download_submissions(canvas, args.course, args.starts_with, args.user)
     
 def getargparser():
     p = localcanvasapi.get_argparser()
     p.description = "List all of the assignments in this course"
     p.add_argument('-c', '--course', required=True, help='The course id whose events will be listed or deleted')
     p.add_argument('--starts_with', required=True, help='downloads all submissions from all assignments that start with this string')
+    p.add_argument('-u', '--user', help='the username of the single user whose submissions are desired')
     return p
 
-def download_submissions(canvas: Canvas, course_id: str, starts_with: str):
+def download_submissions(canvas: Canvas, course_id: str, starts_with: str, user: str):
     course = canvas.get_course(course_id)
     for a in course.get_assignments():
         if(a.name.startswith(starts_with)):
-            download_submissions_for_assignment.download_submissions(canvas, course_id, a.id)
+            download_submissions_for_assignment.download_submissions(canvas, course_id, a.id, user)
     
 if __name__ == '__main__':
    main()
--- download_submissions_for_assignment.py
+++ download_submissions_for_assignment.py
@@ -10,39 +10,40 @@ import localcanvasapi
 def main():
     args = getargparser().parse_args()
     canvas = localcanvasapi.startcanvasapi(args)
-    download_submissions(canvas, args.course, args.assignment)
+    download_submissions(canvas, args.course, args.assignment, args.user)
     
 def getargparser():
     p = localcanvasapi.get_argparser()
     p.description = "Download a zip file with all of the submissions for an assignment in a course"
     p.add_argument('-c', '--course', required=True, help='The course id whose events will be listed or deleted')
     p.add_argument('-a', '--assignment', required=True, help='The assignment id for the submissions desired')
+    p.add_argument('-u', '--user', help='the username of the single user whose submissions are desired')
     return p
 
-def download_submissions(canvas: Canvas, course_id: str, assignment_id: str):
+def download_submissions(canvas: Canvas, course_id: str, assignment_id: str, user=None):
     course = canvas.get_course(course_id)
     assignment = course.get_assignment(assignment_id)
     for s in assignment.get_submissions(include=['user']):
         if(s.attempt):
-            print(vars(s))
-            username = s.user['name']
-            path = Path(f'{assignment.name}/{username}')
-            path.mkdir(parents=True, exist_ok=True) #make a directory with the assignment name and username
-            if(hasattr(s, 'attachments')):
-                for a in s.attachments:
-                    filepath = path.joinpath(a['filename'])
-                    with urllib.request.urlopen(a['url']) as response, open(filepath, 'wb') as out_file:
-                        data = response.read() # a `bytes` object
-                        out_file.write(data)
-                    if(a['mime_class'] == 'zip'):
-                        with zipfile.ZipFile(filepath, 'r') as zip_ref:
-                            zip_ref.extractall(path)
-                        #copy exe, txt, and form1.cs
-                        copy_files_in_tree_to_top(path, "**/bin/**/*.exe")
-                        copy_files_in_tree_to_top(path, "**/bin/**/*.dll")
-                        copy_files_in_tree_to_top(path, "**/bin/**/*.runtimeconfig.json")
-                        copy_files_in_tree_to_top(path, "**/bin/**/*.txt")
-                        copy_files_in_tree_to_top(path, "**/Form1.cs")
+            username = s.user['sortable_name']
+            if(user == None or (user and user.lower() == s.user['login_id'].lower())):
+                path = Path(f'{assignment.name}/{username}')
+                path.mkdir(parents=True, exist_ok=True) #make a directory with the assignment name and username
+                if(hasattr(s, 'attachments')):
+                    for a in s.attachments:
+                        filepath = path.joinpath(a['filename'])
+                        with urllib.request.urlopen(a['url']) as response, open(filepath, 'wb') as out_file:
+                            data = response.read() # a `bytes` object
+                            out_file.write(data)
+                        if(a['mime_class'] == 'zip'):
+                            with zipfile.ZipFile(filepath, 'r') as zip_ref:
+                                zip_ref.extractall(path)
+                            #copy exe, txt, and form1.cs
+                            copy_files_in_tree_to_top(path, "**/bin/**/*.exe")
+                            copy_files_in_tree_to_top(path, "**/bin/**/*.dll")
+                            copy_files_in_tree_to_top(path, "**/bin/**/*.runtimeconfig.json")
+                            copy_files_in_tree_to_top(path, "**/bin/**/*.txt")
+                            copy_files_in_tree_to_top(path, "**/Form1.cs")
 
 def copy_files_in_tree_to_top(path: Path, glob: str):
     for f in list(path.glob(glob)):
