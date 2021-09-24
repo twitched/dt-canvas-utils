41a6d171f5da75bc929d129132409c0f031c80fc
Doug Twitchell <twitched@gmail.com>
2021-09-24 17:56:16 -0600

    allow search term in list assignments

--- list_assignments.py
+++ list_assignments.py
@@ -6,17 +6,20 @@ import localcanvasapi
 def main():
     args = getargparser().parse_args()
     canvas = localcanvasapi.startcanvasapi(args)
-    list_assignments(canvas, args.course)
+    list_assignments(canvas, args.course, args.search_term)
     
 def getargparser():
     p = localcanvasapi.get_argparser()
     p.description = "List all of the assignments in this course"
     p.add_argument('-c', '--course', required=True, help='The course id whose events will be listed or deleted')
+    p.add_argument('-t', '--search_term', help='optional search term to limit which assignments are listed')
     return p
 
-def list_assignments(canvas: Canvas, course_id: str):
+def list_assignments(canvas: Canvas, course_id: str, search_term: str):
     course = canvas.get_course(course_id)
-    for a in course.get_assignments():
+    kwargs = {}
+    kwargs['search_term'] = search_term if search_term else None
+    for a in course.get_assignments(**kwargs):
         print(a)
     
 if __name__ == '__main__':
