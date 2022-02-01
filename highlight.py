a1165a9b7b212ba15264fc88d66c310c365e57db
Doug Twitchell <twitched@gmail.com>
2022-02-01 12:11:45 -0700

    change cs to c# for pygments and allow other style

--- highlight.py
+++ highlight.py
@@ -3,11 +3,11 @@ from pygments.formatters import HtmlFormatter
 from pygments.lexers import (get_lexer_by_name, get_lexer_for_filename)
 from typing import IO
 
-def highlight_file(file: str, type = 'cs'):
+def highlight_file(file: str, type = 'c#', style = 'vs'):
     return highlight_file_io(open(file, mode = 'r'), type)
 
-def highlight_file_io(file: IO, type = 'cs'):
+def highlight_file_io(file: IO, type = 'c#', style = 'vs'):
     return highlight_string(file.read(), type)
 
-def highlight_string(contents: str, type = 'cs'):
-    return highlight(contents, get_lexer_by_name(type), HtmlFormatter(noclasses = True, style = 'vs'))
+def highlight_string(contents: str, type = 'c#', style = 'vs'):
+    return highlight(contents, get_lexer_by_name(type), HtmlFormatter(noclasses = True, style = style))
