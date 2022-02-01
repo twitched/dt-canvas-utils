f7660723ea21c1f30832d1693e2d15c9fae7fb80
Doug Twitchell <twitched@gmail.com>
2022-02-01 12:11:02 -0700

    Change help text to page

--- list_pages.py
+++ list_pages.py
@@ -11,8 +11,8 @@ def main():
 def getargparser():
     p = localcanvasapi.get_argparser()
     p.description = "List all of the assignments in this course"
-    p.add_argument('-c', '--course', required=True, help='The course id whose events will be listed or deleted')
-    p.add_argument('-t', '--search_term', help='optional search term to limit which assignments are listed')
+    p.add_argument('-c', '--course', required=True, help='The course id whose events will be listed')
+    p.add_argument('-t', '--search_term', help='optional search term to limit which pages are listed')
     return p
 
 def get_pages(canvas: Canvas, course_id: str, search_term: str):
